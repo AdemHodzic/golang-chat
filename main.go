aec2c3cc1660ad490e14703dfad0716c6cedb3bf
Adem Hodzic <athos.blade@gmail.com>
2018-09-19 22:04:15 +0200

    Got server working. Need to deploy it.

--- main.go
+++ main.go
@@ -1,31 +1,34 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
-	"github.com/olahol/melody"
+	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/olahol/melody"
 )
 
 func main() {
 
-  r := gin.Default()
-  m := melody.New()
+	r := gin.Default()
+	m := melody.New()
 
-  fs := http.FileServer(http.Dir("static"))
-  http.Handle("/", fs)
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
 
-  r.GET("/", func(c *gin.Context) {
-    http.ServeFile(c.Writer, c.Request, "static/index.html")
-  })
+	r.GET("/", func(c *gin.Context) {
+		http.ServeFile(c.Writer, c.Request, "static/index.html")
+	})
 
-  r.GET("/ws", func(c *gin.Context) {
-    m.HandleRequest(c.Writer, c.Request)
-  })
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
 
-  m.HandleMessage(func(melody *melody.Session, msg []byte) {
-    m.Broadcast(msg)
-  })
+	m.HandleMessage(func(melody *melody.Session, msg []byte) {
+		log.Printf("RECEIVED: %v\n", msg)
+		m.Broadcast(msg)
+	})
 
-  r.Run(":5000")
+	r.Run(":5000")
 
 }
