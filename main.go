a046cd01d749aff878c10d408119ac225ee7b6c6
Adem Hodzic <athos.blade@gmail.com>
2018-09-19 22:25:48 +0200

    Removed unnecessary files

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/olahol/melody"
@@ -12,13 +11,6 @@ func main() {
 
 	r := gin.Default()
 	m := melody.New()
-
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/", fs)
-
-	r.GET("/", func(c *gin.Context) {
-		http.ServeFile(c.Writer, c.Request, "static/index.html")
-	})
 
 	r.GET("/ws", func(c *gin.Context) {
 		m.HandleRequest(c.Writer, c.Request)
